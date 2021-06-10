4370073f658f322f6a7491655c5cf1476cef9614
thiagomayrink <thiagoscmpaiva@gmail.com>
2021-06-10 19:33:06 -0300

    new feature: API users sign-up and providing participants list

--- src/server.js
+++ src/server.js
@@ -5,4 +5,36 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+const users  = [];
+const messages = [];
+
+server.post("/participants", (req,res)=>{
+    const {name} = req.body;
+    if (name===""){
+        return res.sendStatus(400);
+    }
+    const lastStatus = `${Date.now()}`;
+    const user = {
+        name,
+        lastStatus
+    }
+    users.push(user);
+    const date = new Date();
+    const timeHMS = date.toLocaleTimeString('pt-br');
+    const message = {
+        from: user.name,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: timeHMS
+    }
+    messages.push(message);
+    res.sendStatus(200);
+})
+server.get("/participants",(req, res) =>{
+    res.send(users);
+})
+
+
+
 server.listen(4000, ()=>(console.log('Server runnin on port 4000')));
